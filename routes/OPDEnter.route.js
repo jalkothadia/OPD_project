4b128d653605733861d4c4a1c4da608765b3676a
jalkothadia <jalkothadia2217@gmail.com>
2026-02-13 18:39:59 +0530

    Enhance OPDEnter routes with error responses
    Added error handling for get, patch, and delete routes.

--- routes/OPDEnter.route.js
+++ routes/OPDEnter.route.js
@@ -9,21 +9,36 @@ RouterOPDEntery.get('/',async (req,res)=>{
 
 RouterOPDEntery.get('/:id',async (req,res)=>{
     const data = await getById(req.params.id)
+    if(data == 0){
+        res.status(404).json({message:'OPD Entry not exists'})
+    }
     res.send(data)
 })
 
-RouterOPDEntery.post('/:id',async (req,res)=>{
-    const data = await insert(req.params.id)
-    res.send(data)
+RouterOPDEntery.post('/add',async (req,res)=>{
+    const data = await insert(req.body)
+
+    res.status(201).json({
+        error:'false',
+        data,
+        message:'new OPD Entry added'
+    })
 })
 
 RouterOPDEntery.patch('/:id',async (req,res)=>{
-    const data = await update(req.params.id)
+    const data = await update(req.params.id,req.body)
+    if(data == 0){
+        res.status(404).json({message:'OPD Entry not found'})
+    }
     res.send(data)
 })
 
 RouterOPDEntery.delete('/:id',async (req,res)=>{
     const data = await deleteById(req.params.id)
+
+    if(data == 0){
+        res.status(404).json({message:'cannot find OPD Entry'})
+    }
     res.send(data)
 })
 
