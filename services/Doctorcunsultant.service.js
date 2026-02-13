089275421e4903b89aba551efa825d5b3c6c1380
jalkothadia <jalkothadia2217@gmail.com>
2026-02-13 18:42:14 +0530

    Refactor Doctorconsultant service methods with Mongoose

--- services/Doctorcunsultant.service.js
+++ services/Doctorcunsultant.service.js
@@ -1,21 +1,57 @@
+import mongoose from "mongoose"
+import Doctorconsultant from "../models/Doctorconsultant.model.js"
+
+// import { GetAll,GetById,Insert,Delete,Update } from "../models/Doctorconsultant.model.js"
+
 async function getAll() {
-    return `GetAll from service from Diagonsis`
+    const data = await Doctorconsultant.find()
+    return data
 }
 
 async function getById(id){
-    return `GetById  from service Doctor Cunsultant ${id}`
+    const data = await Doctorconsultant.findById(id)
+    return(data)
 }
 
-async function insert(id){
-    return `insert is called  from service Doctor Cunsultant ${id}`
+
+async function insert(formData){
+    try{
+
+        const hos = new Doctorconsultant(formData)
+
+        await hos.validate()
+        await hos.save()
+
+        return(hos)
+    }catch(err){
+        console.log("Insert err" , err)
+    }
 }
 
-async function update(id){
-    return `update is called  from service Doctor Cunsultant ${id}`
+async function update(id, formData){
+   try{
+        const editHospital = await Doctorconsultant.findByIdAndUpdate(id,formData,{new:true})
+        if(!editHospital){
+            return 0
+        }
+        return(editHospital)
+    }catch(err){
+        console.log("Edit Error : ",err)
+    }
 }
 
 async function deleteById(id){
-    return `delete is called  from service Doctor Cunsultant ${id}`
+   try{
+        const DeletedHospital = await Doctorconsultant.findByIdAndDelete(id)
+
+        if(!DeletedHospital){
+            return 0
+        }
+
+        return (DeletedHospital)
+    }catch(err){
+        console.log("Error : ",err)
+    }
 }
 
 export {getAll,getById,insert,update,deleteById}
