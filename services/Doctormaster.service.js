29a5d56c8a1051d9b24559fc1e5ee3f0b409e793
jalkothadia <jalkothadia2217@gmail.com>
2026-02-13 18:42:36 +0530

    Refactor Doctor service functions with Mongoose

--- services/Doctormaster.service.js
+++ services/Doctormaster.service.js
@@ -1,21 +1,57 @@
+import mongoose from "mongoose"
+import Doctormaster from "../models/Doctormaster.model.js"
+
 async function getAll() {
-    return `GetAll from service from Doctor Master`
+    const data = await Doctormaster.find() 
+    return data
 }
 
-async function getById(id){
-    return `GetById  from service Doctor Master ${id}`
+async function getById(_id){
+    const data = await Doctormaster.findById(_id)
+    if(!data){
+        return 0
+    }
+    return(data)
 }
 
-async function insert(id){
-    return `insert is called  from service Doctor Master ${id}`
+async function insert(formData){
+    try{
+
+        const doc = new Doctormaster(formData)
+
+        await doc.validate()
+        await doc.save()
+
+        return(doc)
+    }catch(err){
+        console.log("Insert err" , err)
+    }
 }
 
-async function update(id){
-    return `update is called  from service Doctor Master ${id}`
+async function update(id,formData){
+    try{
+        const editDoctor = await Doctormaster.findByIdAndUpdate(id,formData,{new:true})
+        if(!editDoctor){
+            return 0
+        }
+        return(editDoctor)
+    }catch(err){
+        console.log("Edit Error : ",err)
+    }
 }
 
 async function deleteById(id){
-    return `delete is called  from service Doctor Master ${id}`
+    try{
+        const DeletedDoctor = await Doctormaster.findByIdAndDelete(id)
+
+        if(!DeletedDoctor){
+            return 0
+        }
+
+        return (DeletedDoctor)
+    }catch(err){
+        console.log("Error : ",err)
+    }
 }
 
 export {getAll,getById,insert,update,deleteById}
