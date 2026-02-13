43b5bfe6f8d3f138ead86c8b804cd8a7813e547d
jalkothadia <jalkothadia2217@gmail.com>
2026-02-13 18:44:29 +0530

    Implement CRUD operations using Mongoose
    Refactor Treatmentmaster service to use Mongoose for database operations.

--- services/Treatmentmaster.service.js
+++ services/Treatmentmaster.service.js
@@ -1,21 +1,56 @@
+import mongoose from "mongoose"
+import Treatment from '../models/Treatment.model.js'
+
+// import { GetAll,GetById,Insert,Update,Delete } from "../models/Treatment.model.js"
+
 async function getAll() {
-    return `GetAll from service from TreatmentMaster`
+   const data = await Treatment.find()
+    return data
 }
 
 async function getById(id){
-    return `GetById  from service TreatmentMaster ${id}`
+    const data = await Treatment.findById(id)
+    return(data)
 }
 
-async function insert(id){
-    return `insert is called  from service TreatmentMaster ${id}`
+async function insert(formData){
+   try{
+
+        const tre = new Treatment(formData)
+
+        await tre.validate()
+        await tre.save()
+
+        return(tre)
+    }catch(err){
+        console.log("Insert err" , err)
+    }
 }
 
-async function update(id){
-    return `update is called  from service TreatmentMaster ${id}`
+async function update(id ,formData){
+    try{
+        const editTreatment = await Treatment.findByIdAndUpdate(id,formData,{new:true})
+        if(!editTreatment){
+            return 0
+        }
+        return(editTreatment)
+    }catch(err){
+        console.log("Edit Error : ",err)
+    }
 }
 
 async function deleteById(id){
-    return `delete is called  from service TreatmentMaster ${id}`
+    try{
+        const DeletedTreatment = await Treatment.findByIdAndDelete(id)
+
+        if(!DeletedTreatment){
+            return 0
+        }
+
+        return (DeletedTreatment)
+    }catch(err){
+        console.log("Error : ",err)
+    }
 }
 
 export {getAll,getById,insert,update,deleteById}
