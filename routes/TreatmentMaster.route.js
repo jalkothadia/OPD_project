8c57f2b3b9826da6d29778cff41e94f86a4cce1b
jalkothadia <jalkothadia2217@gmail.com>
2026-02-13 18:41:05 +0530

    Enhance TreatmentMaster routes with error handling

--- routes/TreatmentMaster.route.js
+++ routes/TreatmentMaster.route.js
@@ -9,21 +9,36 @@ RouterTreatmentMaster.get('/',async (req,res)=>{
 
 RouterTreatmentMaster.get('/:id',async (req,res)=>{
     const data = await getById(req.params.id)
+    if(data == 0){
+        res.status(404).json({message:'Doctor not exists'})
+    }
     res.send(data)
 })
 
-RouterTreatmentMaster.post('/:id',async (req,res)=>{
-    const data = await insert(req.params.id)
-    res.send(data)
+RouterTreatmentMaster.post('/add',async (req,res)=>{
+    const data = await insert(req.body)
+
+    res.status(201).json({
+        error:'false',
+        data,
+        message:'new doctor added'
+    })
 })
 
 RouterTreatmentMaster.patch('/:id',async (req,res)=>{
-    const data = await update(req.params.id)
+    const data = await update(req.params.id,req.body)
+    if(data == 0){
+        res.status(404).json({message:'Doctor not found'})
+    }
     res.send(data)
 })
 
 RouterTreatmentMaster.delete('/:id',async (req,res)=>{
     const data = await deleteById(req.params.id)
+
+    if(data == 0){
+        res.status(404).json({message:'cannot find doctor'})
+    }
     res.send(data)
 })
 export default RouterTreatmentMaster
