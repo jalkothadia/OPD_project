e60c8c21004bf1f6ed77b04f551c86347457cace
jalkothadia <jalkothadia2217@gmail.com>
2026-02-13 18:38:56 +0530

    Update Doctormaster.route.js

--- routes/Doctormaster.route.js
+++ routes/Doctormaster.route.js
@@ -9,21 +9,36 @@ RouterDoctorMaster.get('/',async (req,res)=>{
 
 RouterDoctorMaster.get('/:id',async (req,res)=>{
     const data = await getById(req.params.id)
+    if(data == 0){
+        res.status(404).json({message:'Doctor not exists'})
+    }
     res.send(data)
 })
 
-RouterDoctorMaster.post('/:id',async (req,res)=>{
-    const data = await insert(req.params.id)
-    res.send(data)
+RouterDoctorMaster.post('/add',async (req,res)=>{
+    const data = await insert(req.body)
+
+    res.status(201).json({
+        error:'false',
+        data,
+        message:'new doctor added'
+    })
 })
 
 RouterDoctorMaster.patch('/:id',async (req,res)=>{
-    const data = await update(req.params.id)
+    const data = await update(req.params.id,req.body)
+    if(data == 0){
+        res.status(404).json({message:'Doctor not found'})
+    }
     res.send(data)
 })
 
 RouterDoctorMaster.delete('/:id',async (req,res)=>{
     const data = await deleteById(req.params.id)
+
+    if(data == 0){
+        res.status(404).json({message:'cannot find doctor'})
+    }
     res.send(data)
 })
 
