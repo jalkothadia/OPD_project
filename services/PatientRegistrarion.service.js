9fec4ed56a7304c014c01c3fbe3d6c29610dd2e6
jalkothadia <jalkothadia2217@gmail.com>
2026-02-13 18:43:49 +0530

    Update PatientRegistrarion.service.js

--- services/PatientRegistrarion.service.js
+++ services/PatientRegistrarion.service.js
@@ -1,21 +1,67 @@
+import mongoose from "mongoose"
+import patients from '../models/PatientRegistration.model.js'
+// import { GetAll,GetById,Insert,Delete,Update } from "../models/PatientRegistration.model.js"
+
 async function getAll() {
-    return `GetAll from service from PatientRegistration`
+    const data = await patients.find() 
+    if(data){
+        return{
+            error : false,
+            data,
+            message : "data fetched success"
+        }
+    }
+    else{
+        return{
+            error : true,
+            message : "Error occured"
+        }
+    }
 }
 
 async function getById(id){
-    return `GetById  from service PatientRegistration ${id}`
+    const data = await patients.findById(id)
+    return(data)
 }
 
-async function insert(id){
-    return `insert is called  from service PatientRegistration ${id}`
+async function insert(formData){
+    try{
+
+        const newpatient = new patients(formData)
+
+        await newpatient.validate()
+        await newpatient.save()
+
+        return(newpatient)
+    }catch(err){
+        console.log("Insert err" , err)
+    }
 }
 
-async function update(id){
-    return `update is called  from service PatientRegistration ${id}`
+async function update(id, formData){
+    try{
+        const editPatient = await patients.findByIdAndUpdate(id,formData,{new:true})
+        if(!editPatient){
+            return 0
+        }
+        return(editPatient)
+    }catch(err){
+        console.log("Edit Error : ",err)
+    }
 }
 
 async function deleteById(id){
-    return `delete is called  from service PatientRegistration ${id}`
+    try{
+        const DeletedPatient = await patients.findByIdAndDelete(id)
+
+        if(!DeletedPatient){
+            return 0
+        }
+
+        return (DeletedPatient)
+    }catch(err){
+        console.log("Error : ",err)
+    }
 }
 
 export {getAll,getById,insert,update,deleteById}
