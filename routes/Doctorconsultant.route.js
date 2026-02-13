5e67fe54bf8f3efa3e89d4e9eb76a1b149fa9d50
jalkothadia <jalkothadia2217@gmail.com>
2026-02-13 18:37:27 +0530

    Enhance DoctorConsultant routes with error handling

--- routes/Doctorconsultant.route.js
+++ routes/Doctorconsultant.route.js
@@ -9,21 +9,36 @@ RouterDoctorConsultant.get('/',async (req,res)=>{
 
 RouterDoctorConsultant.get('/:id',async (req,res)=>{
     const data = await getById(req.params.id)
+    if(data == 0){
+        res.status(404).json({message:'Doctor not exists'})
+    }
     res.send(data)
 })
 
-RouterDoctorConsultant.post('/:id',async (req,res)=>{
-    const data = await insert(req.params.id)
-    res.send(data)
+RouterDoctorConsultant.post('/add',async (req,res)=>{
+    const data = await insert(req.body)
+
+    res.status(201).json({
+        error:'false',
+        data,
+        message:'new doctor added'
+    })
 })
 
 RouterDoctorConsultant.patch('/:id',async (req,res)=>{
-    const data = await update(req.params.id)
+    const data = await update(req.params.id,req.body)
+    if(data == 0){
+        res.status(404).json({message:'Doctor not found'})
+    }
     res.send(data)
 })
 
 RouterDoctorConsultant.delete('/:id',async (req,res)=>{
     const data = await deleteById(req.params.id)
+
+    if(data == 0){
+        res.status(404).json({message:'cannot find doctor'})
+    }
     res.send(data)
 })
 
