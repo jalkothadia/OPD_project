87b8e40c82da90778af926a491b2ab9411c917fd
jalkothadia <jalkothadia2217@gmail.com>
2026-02-13 18:44:12 +0530

    Refactor SubTreatment service to use mongoose

--- services/SubTreatment.service.js
+++ services/SubTreatment.service.js
@@ -1,21 +1,56 @@
+import mongoose from "mongoose"
+import Subtreatment from "../models/SubTreatment.model.js"
+
+// import { GetAll,GetById,Insert,Delete,Update } from "../models/SubTreatment.model.js"
+
 async function getAll() {
-    return `GetAll from service from SubTreatment`
+    const data = await Subtreatment.find()
+    return data
 }
 
-async function getById(id){
-    return `GetById  from service SubTreatment ${id}`
+async function getById(id) {
+     const data = await Subtreatment.findById(id)
+    return(data)
 }
 
-async function insert(id){
-    return `insert is called  from service SubTreatment ${id}`
+async function insert(formData) {
+    try{
+
+        const subtre = new Subtreatment(formData)
+
+        await subtre.validate()
+        await subtre.save()
+
+        return(subtre)
+    }catch(err){
+        console.log("Insert err" , err)
+    }
 }
 
-async function update(id){
-    return `update is called  from service SubTreatment ${id}`
+async function update(id,formData) {
+    try{
+        const editSubTre = await Subtreatment.findByIdAndUpdate(id,formData,{new:true})
+        if(!editSubTre){
+            return 0
+        }
+        return(editSubTre)
+    }catch(err){
+        console.log("Edit Error : ",err)
+    }
 }
 
-async function deleteById(id){
-    return `delete is called  from service SubTreatment ${id}`
+async function deleteById(id) {
+    try{
+        const DeletedSubTre = await Subtreatment.findByIdAndDelete(id)
+
+        if(!DeletedSubTre){
+            return 0
+        }
+
+        return (DeletedSubTre)
+    }catch(err){
+        console.log("Error : ",err)
+    }
 }
 
-export {getAll,getById,insert,update,deleteById}
+export { getAll, getById, insert, update, deleteById }
