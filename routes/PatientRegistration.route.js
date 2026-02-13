8facf14c7ed50b1c86c5187fdd25e96b03731924
jalkothadia <jalkothadia2217@gmail.com>
2026-02-13 18:40:20 +0530

    Enhance patient registration routes with error handling

--- routes/PatientRegistration.route.js
+++ routes/PatientRegistration.route.js
@@ -9,21 +9,36 @@ RouterPatientRegistrarion.get('/',async (req,res)=>{
 
 RouterPatientRegistrarion.get('/:id',async (req,res)=>{
     const data = await getById(req.params.id)
+    if(data == 0){
+        res.status(404).json({message:'Patient not exists'})
+    }
     res.send(data)
 })
 
-RouterPatientRegistrarion.post('/:id',async (req,res)=>{
-    const data = await insert(req.params.id)
-    res.send(data)
+RouterPatientRegistrarion.post('/add',async (req,res)=>{
+    const data = await insert(req.body)
+
+    res.status(201).json({
+        error:'false',
+        data,
+        message:'new doctor added'
+    })
 })
 
 RouterPatientRegistrarion.patch('/:id',async (req,res)=>{
-    const data = await update(req.params.id)
+    const data = await update(req.params.id,req.body)
+    if(data == 0){
+        res.status(404).json({message:'Patient not found'})
+    }
     res.send(data)
 })
 
 RouterPatientRegistrarion.delete('/:id',async (req,res)=>{
     const data = await deleteById(req.params.id)
+
+    if(data == 0){
+        res.status(404).json({message:'cannot find Patient'})
+    }
     res.send(data)
 })
 
