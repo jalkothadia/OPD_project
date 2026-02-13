22137c00365b87beb4181cf48d4512aee7a276e2
jalkothadia <jalkothadia2217@gmail.com>
2026-02-13 18:43:34 +0530

    Refactor OPDEntry service to use Mongoose
    Refactored OPDEntry service functions to use Mongoose for database operations, including getAll, getById, insert, update, and deleteById.

--- services/OPDEntry.service.js
+++ services/OPDEntry.service.js
@@ -1,21 +1,55 @@
+import mongoose from "mongoose"
+import OPD from '../models/OPDEntery.model.js'
+// import { GetAll,GetById,Insert,Update,Delete } from "../models/OPDEntery.model.js"
+
 async function getAll() {
-    return `GetAll from service from OPDEntry`
+    const data = await OPD.find() 
+    return data
 }
 
 async function getById(id){
-    return `GetById  from service OPDEntry ${id}`
+    const data = await OPD.findById(id)
+    return(data)
 }
 
-async function insert(id){
-    return `insert is called  from service OPDEntry ${id}`
+async function insert(formData){
+    try{
+
+        const opd = new OPD(formData)
+
+        await opd.validate()
+        await opd.save()
+
+        return(opd)
+    }catch(err){
+        console.log("Insert err" , err)
+    }
 }
 
-async function update(id){
-    return `update is called  from service OPDEntry ${id}`
+async function update(id,formData){
+    try{
+        const editOPD = await OPD.findByIdAndUpdate(id,formData,{new:true})
+        if(!editOPD){
+            return 0
+        }
+        return(editOPD)
+    }catch(err){
+        console.log("Edit Error : ",err)
+    }
 }
 
 async function deleteById(id){
-    return `delete is called  from service OPDEntry ${id}`
+    try{
+        const DeletedOPD = await OPD.findByIdAndDelete(id)
+
+        if(!DeletedOPD){
+            return 0
+        }
+
+        return (DeletedOPD)
+    }catch(err){
+        console.log("Error : ",err)
+    }
 }
 
 export {getAll,getById,insert,update,deleteById}
