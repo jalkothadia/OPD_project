33f28a866d9629a20eefaedaf14af8811725eb4e
jalkothadia <jalkothadia2217@gmail.com>
2026-02-13 18:40:47 +0530

    Enhance SubTreatment routes with error handling
    Added error handling for get, update, and delete routes. Modified post route to accept request body and return a success message.

--- routes/SubTreatment.route.js
+++ routes/SubTreatment.route.js
@@ -9,21 +9,36 @@ RouterSubTreatment.get('/',async (req,res)=>{
 
 RouterSubTreatment.get('/:id',async (req,res)=>{
     const data = await getById(req.params.id)
+    if(data == 0){
+        res.status(404).json({message:'SubTreatment not exists'})
+    }
     res.send(data)
 })
 
-RouterSubTreatment.post('/:id',async (req,res)=>{
-    const data = await insert(req.params.id)
-    res.send(data)
+RouterSubTreatment.post('/add',async (req,res)=>{
+    const data = await insert(req.body)
+
+    res.status(201).json({
+        error:'false',
+        data,
+        message:'new SubTreatment added'
+    })
 })
 
 RouterSubTreatment.patch('/:id',async (req,res)=>{
-    const data = await update(req.params.id)
+   const data = await update(req.params.id,req.body)
+    if(data == 0){
+        res.status(404).json({message:'SubTreatment not found'})
+    }
     res.send(data)
 })
 
 RouterSubTreatment.delete('/:id',async (req,res)=>{
     const data = await deleteById(req.params.id)
+
+    if(data == 0){
+        res.status(404).json({message:'cannot find Subtreatment'})
+    }
     res.send(data)
 })
 
